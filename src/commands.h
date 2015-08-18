1df56e86d9830eacf60c43bbdc4fe39d2e946115
Yohan Joubert <yohan.joubert@gmail.com>
2015-08-18 14:25:35 +0200

    Usage written. Some doc in header file.

--- src/commands.h
+++ src/commands.h
@@ -8,9 +8,24 @@
 #include "keyring.h"
 #include "item.h"
 
+/*
+  Checks that options are correctly set and launches the keyring creation function.
+*/
 int execute_create(const s_option *opt);
+
+/*
+  Checks that options are correctly set and launches the keyring listing function.
+*/
 int execute_list(const s_option *opt);
+
+/*
+  Checks that options are correctly set and launches the store password function.
+*/
 int execute_store(const s_option *opt);
+
+/*
+  Checks that options are correctly set and launches the retrieve password function.
+*/
 int execute_get(const s_option *opt);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 #include "main.h"
 
+char *prg_name = "";
+
 void help()
 {
-  fprintf(stderr, "TODO\n");
+  fprintf(stderr, "Usage: %s command [OPTIONS]...\n", prg_name);
+  fprintf(stderr, "Commands:\n");
+  fprintf(stderr, "\tlist\tDisplay a list of the existing keyrings\n");
+  fprintf(stderr, "\tcreate -k keyring [-p password]\tCreate a new keyring.\n");
+  fprintf(stderr, "\tstore -k keyring -n name [-p password] -a attributes...\tStore a password in a keyring\n");
+  fprintf(stderr, "\tget -k keyring -a key:value...\tRetrieve a password from a keyring that matches the key/value couples.\n");
+
+  fprintf(stderr, "\n");
+
+  fprintf(stderr, "Options:\n");
+  fprintf(stderr, "\t-a --attributes\tA couple used to identify a keyring item. Must be in the form key:value\n");
+  fprintf(stderr, "\t-k --keyring\tA keyring name. Represents the new name of the keyring in the create command, identifies the keyring we want to use otherwise.\n");
+  fprintf(stderr, "\t-n --name\tThe display name of the keyring item. It can't be used to retrieve the password.\n");
+  fprintf(stderr, "\t-p --password\tOnly used when creating a new keyring or item. If not provided, the user will be prompted for the password.\n");
 }
 
 int execute_command(const char *command, const s_option *opt)
@@ -57,18 +72,20 @@ int main(int argc, char* const argv[])
   int index;
   int c;
   int result = 0;
+  prg_name = argv[0];
+
   s_option *opt = malloc(sizeof(s_option));
   init_options(opt);
 
   static struct option long_options[] = {
         {"attributes",  required_argument, 0, 'a' },
         {"keyring",     required_argument, 0, 'k' },
-        {"password",    required_argument, 0, 'p' },
         {"name",        required_argument, 0, 'n' },
-        {0,             0,                 0,  0   }
+        {"password",    required_argument, 0, 'p' },
+        {0,             0,                 0,  0  }
     };
 
-  while ((c = getopt_long(argc, argv, "a:k:p:n:", long_options, NULL)) != -1)
+  while ((c = getopt_long(argc, argv, "a:k:n:p:", long_options, NULL)) != -1)
     switch (c)
     {
       case 'a':
